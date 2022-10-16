a07feb1b9ec83057666a336dd1c7314d5ea18a7a
Keenan <keenancberg@gmail.com>
2022-10-16 19:21:35 +0300

    Add filledCell class & mouseover event listener. Add change grid size button - not functional yet.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # etch-a-sketch
+
+The Odin Project assigment is to make a game of Etch-a-Sketch using a combination of the html, css, and javascript skills I've been taught so far. 
--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const container = document.querySelector("#container");
 
+
 const grid = document.createElement("div");
 grid.classList.add("grid")
 container.appendChild(grid);
 
+const cell = document.createElement("div");
+cell.classList.add("cell");
 
+const btn1 = document.createElement("button");
+btn1.classList.add("button");
+btn1.textContent = "Change grid size"
+button1.appendChild(btn1);
+
+btn1.addEventListener("click", () => {
+    let userInput = prompt("How large would you like the grid? (1-100")
+    makeGrid(userInput)
+})
+
+function clear(){
+        cell.classList.remove("filledCell")
+};
 
 function makeGrid(gridUnits){
     for (let i= 0; i < (gridUnits * gridUnits); i++){
@@ -14,3 +30,19 @@ function makeGrid(gridUnits){
 }
 
 makeGrid(16)
+
+const cells = document.querySelectorAll(".cell");
+cells.forEach(cell => cell.addEventListener("mouseover", () => {
+    cell.classList.add("filledCell")
+}));
+
+
+
+//cells.forEach(cell => cell.addEventListener("mouseover", colorCell));
+
+//addEventListener("mouseover", colorCell);
+
+//function colorCell(){
+//    cell.style.backgroundColor = "blue";
+
+//}
